2637310a8137c09892a4f18daeca917d85f2bf23
Nicolas Andrade de Freitas <nicolasandrade.de.freitas@gmail.com>
2023-04-06 16:31:39 -0300

    fix the app.module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { AppService } from './app.service';
+import { UserEntity } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() currentUser: UserEntity) {
+    console.log(currentUser);
+    return currentUser;
   }
 }
